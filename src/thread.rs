27f47e44aa11c3f7487cbea85e9a8ab972d0c5b2
HansiHE <hansihe@hansihe.com>
2017-01-09 15:10:54 +0100

    move thread spawn method into spawner trait

--- src/thread.rs
+++ src/thread.rs
@@ -15,59 +15,59 @@ fn caller<'a>(caller_env: NifEnv<'a>) -> ErlNifPid {
     pid
 }
 
-pub trait JobSpawner {
-    fn spawn<F: FnOnce() + Send + panic::UnwindSafe + 'static>(job: F);
+pub trait AsyncSend {
+    /// Implements threaded NIFs.
+    ///
+    /// This spawns a thread that calls the given closure `thread_fn`. When the closure returns, the
+    /// thread sends its return value back to the calling process.  If the closure panics, an `{error,
+    /// Reason}` tuple is sent instead.
+    ///
+    /// Note that the thread creates a new `NifEnv` and passes it to the closure, so the closure
+    /// runs under a separate environment, not under `env`.
+    ///
+    fn spawn<'a, F>(env: NifEnv<'a>, thread_fn: F)
+        where F: for<'b> FnOnce(NifEnv<'b>) -> NifTerm<'b> + Send + panic::UnwindSafe + 'static,
+    {
+        let pid = caller(env);
+
+        Self::spawn_job(move || {
+            let env = NifEnv {
+                env: unsafe { nif_interface::enif_alloc_env() },
+                id: PhantomData
+            };
+
+            let message = match panic::catch_unwind(|| thread_fn(env)) {
+                Ok(term) => term,
+                Err(err) => {
+                    // Try to get an error message from Rust.
+                    let reason =
+                        if let Some(string) = err.downcast_ref::<String>() {
+                            string.encode(env)
+                        } else if let Some(&s) = err.downcast_ref::<&'static str>() {
+                            s.encode(env)
+                        } else {
+                            atom::get_atom_init("nif_panic").to_term(env)
+                        };
+                    env.error_tuple(reason)
+                }
+            };
+
+            unsafe {
+                nif_interface::enif_send(env.as_c_arg(),
+                                         &pid,
+                                         env.as_c_arg(),
+                                         message.as_c_arg());
+            }
+        });
+    }
+
+    fn spawn_job<F: FnOnce() + Send + panic::UnwindSafe + 'static>(job: F);
 }
 
-pub struct ThreadSpawner;
-impl JobSpawner for ThreadSpawner {
-    fn spawn<F: FnOnce() + Send + panic::UnwindSafe + 'static>(job: F) {
+pub struct ThreadAsyncSend;
+impl AsyncSend for ThreadAsyncSend {
+    fn spawn_job<F: FnOnce() + Send + panic::UnwindSafe + 'static>(job: F) {
         thread::spawn(job);
     }
 }
 
-/// Implements threaded NIFs.
-///
-/// This spawns a thread that calls the given closure `thread_fn`. When the closure returns, the
-/// thread sends its return value back to the calling process.  If the closure panics, an `{error,
-/// Reason}` tuple is sent instead.
-///
-/// Note that the thread creates a new `NifEnv` and passes it to the closure, so the closure
-/// runs under a separate environment, not under `env`.
-///
-pub fn spawn<'a, S, F>(env: NifEnv<'a>, thread_fn: F)
-    where F: for<'b> FnOnce(NifEnv<'b>) -> NifTerm<'b> + Send + panic::UnwindSafe + 'static,
-          S: JobSpawner,
-{
-    let pid = caller(env);
-
-    S::spawn(move || {
-        let env = NifEnv {
-            env: unsafe { nif_interface::enif_alloc_env() },
-            id: PhantomData
-        };
-
-        let message = match panic::catch_unwind(|| thread_fn(env)) {
-            Ok(term) => term,
-            Err(err) => {
-                // Try to get an error message from Rust.
-                let reason =
-                    if let Some(string) = err.downcast_ref::<String>() {
-                        string.encode(env)
-                    } else if let Some(&s) = err.downcast_ref::<&'static str>() {
-                        s.encode(env)
-                    } else {
-                        atom::get_atom_init("nif_panic").to_term(env)
-                    };
-                env.error_tuple(reason)
-            }
-        };
-
-        unsafe {
-            nif_interface::enif_send(env.as_c_arg(),
-                                     &pid,
-                                     env.as_c_arg(),
-                                     message.as_c_arg());
-        }
-    });
-}
--- test/src/test_thread.rs
+++ test/src/test_thread.rs
@@ -1,5 +1,6 @@
 use rustler::{ NifEnv, NifTerm, NifResult, NifEncoder, NifDecoder };
 use rustler::thread;
+use rustler::thread::AsyncSend;
 use std;
 
 pub fn threaded_fac<'a>(env: NifEnv<'a>, args: &Vec<NifTerm>) -> NifResult<NifTerm<'a>> {
@@ -11,7 +12,7 @@ pub fn threaded_fac<'a>(env: NifEnv<'a>, args: &Vec<NifTerm>) -> NifResult<NifTe
     }
 
     let n: u64 = args[0].decode()?;
-    thread::spawn::<thread::ThreadSpawner, _>(env, move |thread_env| {
+    thread::ThreadAsyncSend::spawn(env, move |thread_env| {
         let result = (1 .. n + 1).fold(1, mul);
         result.encode(thread_env)
     });
@@ -24,7 +25,7 @@ pub fn threaded_sleep<'a>(env: NifEnv<'a>, args: &Vec<NifTerm>) -> NifResult<Nif
 
     let q = msec / 1000;
     let r = (msec % 1000) as u32;
-    thread::spawn::<thread::ThreadSpawner, _>(env, move |thread_env| {
+    thread::ThreadAsyncSend::spawn(env, move |thread_env| {
         std::thread::sleep(std::time::Duration::new(q as u64, r * 1_000_000));
         msec.encode(thread_env)
     });
