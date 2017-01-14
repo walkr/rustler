8d6f856efdbd784279c19b69ab1bc98930acfa2c
HansiHE <hansihe@hansihe.com>
2017-01-14 18:01:16 +0100

    first mockup of specific encode traits

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,3 +1,24 @@
+#[macro_export]
+macro_rules! default_encode {
+    ($typ:ty, $target:path) => {
+        impl NifEncoder for $typ {
+            fn encode<'b>(&self, env: NifEnv<'b>) -> NifTerm<'b>{
+                SpecificEncode::<$target>::encode(self, env)
+            }
+        }
+    }
+}
+#[macro_export]
+macro_rules! default_decode {
+    ($typ:ty, $target:path) => {
+        impl<'a> NifDecoder<'a> for $typ {
+            fn decode(term: NifTerm<'a>) -> NifResult<$typ> {
+                SpecificDecode::<'a, $target>::decode(term)
+            }
+        }
+    }
+}
+
 use ::{
     NifEnv,
     NifTerm,
@@ -31,4 +52,34 @@ impl<'a> NifDecoder<'a> for NifTerm<'a> {
     fn decode(term: NifTerm<'a>) -> NifResult<Self> {
         Ok(term)
     }
+}
+
+
+pub trait TranscodeTarget {}
+
+macro_rules! define_target {
+    ($name:ident) => {
+        pub struct $name;
+        impl $crate::types::TranscodeTarget for $name {}
+    }
+}
+
+pub mod target {
+    define_target!(Atom);
+    define_target!(Binary);
+    define_target!(List);
+    define_target!(Map);
+    define_target!(Tuple);
+
+    define_target!(Resource);
+    define_target!(KeywordList);
+    define_target!(ElixirStruct);
+}
+
+
+pub trait SpecificEncode<'a, Target> where Target: TranscodeTarget {
+    fn encode(&self, env: NifEnv<'a>) -> NifTerm<'a>;
+}
+pub trait SpecificDecode<'a, Target>: Sized + 'a where Target: TranscodeTarget {
+    fn decode(term: NifTerm<'a>) -> NifResult<Self>;
 }
--- src/types/string.rs
+++ src/types/string.rs
@@ -1,26 +1,17 @@
 use ::{ NifTerm, NifEnv, NifEncoder, NifDecoder, NifResult, NifError };
 use super::binary::{ NifBinary, OwnedNifBinary };
 
-impl<'a> NifDecoder<'a> for String {
-    fn decode(term: NifTerm<'a>) -> NifResult<Self> {
-        let string: &str = try!(NifDecoder::decode(term));
-        Ok(string.to_string())
-    }
-}
-impl<'a> NifDecoder<'a> for &'a str {
-    fn decode(term: NifTerm<'a>) -> NifResult<Self> {
-        let binary = try!(NifBinary::from_term(term));
-        match ::std::str::from_utf8(binary.as_slice()) {
-            Ok(string) => Ok(string),
-            Err(_) => Err(NifError::BadArg),
-        }
-    }
-}
+use super::{ SpecificEncode, SpecificDecode };
+use super::target;
 
 use std::io::Write;
 
-impl NifEncoder for str {
-    fn encode<'b>(&self, env: NifEnv<'b>) -> NifTerm<'b> {
+#[cfg(not(erlang))]
+default_encode!(str, target::Binary);
+#[cfg(erlang)]
+default_encode!(str, target::List);
+impl<'a> SpecificEncode<'a, target::Binary> for str {
+    fn encode(&self, env: NifEnv<'a>) -> NifTerm<'a> {
         let str_len = self.len();
         let mut bin = match OwnedNifBinary::alloc(str_len) {
             Some(bin) => bin,
@@ -28,5 +19,30 @@ impl NifEncoder for str {
         };
         bin.as_mut_slice().write(self.as_bytes()).expect("memory copy of string failed");
         bin.release(env).get_term(env)
+    }
+}
+
+#[cfg(not(erlang))]
+default_decode!(String, target::Binary);
+#[cfg(erlang)]
+default_decode!(String, target::List);
+impl<'a> SpecificDecode<'a, target::Binary> for String {
+    fn decode(term: NifTerm<'a>) -> NifResult<Self> {
+        let string: &str = try!(NifDecoder::decode(term));
+        Ok(string.to_string())
+    }
+}
+
+#[cfg(not(erlang))]
+default_decode!(&'a str, target::Binary);
+#[cfg(erlang)]
+default_decode!(&'a str, target::List);
+impl<'a> SpecificDecode<'a, target::Binary> for &'a str {
+    fn decode(term: NifTerm<'a>) -> NifResult<Self> {
+        let binary = try!(NifBinary::from_term(term));
+        match ::std::str::from_utf8(binary.as_slice()) {
+            Ok(string) => Ok(string),
+            Err(_) => Err(NifError::BadArg),
+        }
     }
 }
